7bda3371f3f2e959048560a25ea509d618a6e9eb
Saw <lawdrel@gmail.com>
2018-10-01 17:57:55 +0300

    redux todolist

--- todo-list-redux/src/containers/App.js
+++ todo-list-redux/src/containers/App.js
@@ -6,7 +6,7 @@ export default class extends React.Component {
     render() {
         return (
             <div className='container'>
-                <h1>ToDo List</h1>
+                <h1>ToDo List with Redux</h1>
                 <Tasks />
                 {/*<Users test='hello' />*/}
             </div>
